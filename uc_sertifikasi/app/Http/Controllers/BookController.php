99b1a143e31b16c4001d25fb2300523dd629ddb6
Bryant97-dev <62684129+Bryant97-dev@users.noreply.github.com>
2022-12-18 20:49:40 +0800

    two
    two

--- uc_sertifikasi/app/Http/Controllers/BookController.php
+++ uc_sertifikasi/app/Http/Controllers/BookController.php
@@ -24,52 +24,51 @@ class BookController extends Controller
     
     }
 
-    // method untuk insert data ke table pegawai
     public function add(Request $request)
     {
-        // insert data ke table pegawai
+  
         DB::table('book')->insert([
             'title' => $request->title,
             'author' => $request->author,
             'page' => $request->page,
             'publication_year' => $request->publication_year
         ]);
-        // alihkan halaman ke halaman pegawai
+    
         return redirect('/book');
     
     }
 
     public function update(Request $request)
     {
-        // update data pegawai
+   
         DB::table('book')->where('book_id',$request->id)->update([
             'title' => $request->title,
             'author' => $request->author,
             'page' => $request->page,
             'publication_year' => $request->publication_year
         ]);
-        // alihkan halaman ke halaman pegawai
+   
         return redirect('/book');
     }
 
-    // method untuk edit data pegawai
+   
     public function editbook($id)
     {
-        // mengambil data pegawai berdasarkan id yang dipilih
+       
         $book = DB::table('book')->where('book_id',$id)->get();
         $list = DB::table('book')->get();
-        // passing data pegawai yang didapat ke view edit.blade.php
+     
         return view('editbook',['book' => $book,'list'=>$list]);
     
     }
 
-    // method untuk hapus data pegawai
+ 
     public function delete($id)
     {
-        // menghapus data pegawai berdasarkan id yang dipilih
+       
         DB::table('book')->where('book_id',$id)->delete();
             
-        // alihkan halaman ke halaman pegawai
+     
         return redirect('/book');
     }
 
--- uc_sertifikasi/app/Http/Controllers/LoanController.php
+++ uc_sertifikasi/app/Http/Controllers/LoanController.php
@@ -30,7 +30,7 @@ class LoanController extends Controller
 
     public function add(Request $request)
     {
-        // insert data ke table pegawai
+       
         DB::table('loan')->insert([
             'title' => $request->title,
             'member' => $request->member,
@@ -39,40 +39,40 @@ class LoanController extends Controller
         ]);
 
       
-        // alihkan halaman ke halaman pegawai
+      
         return redirect('/loan');
     
     }
 
     public function editloan($id)
     {
-        // mengambil data pegawai berdasarkan id yang dipilih
+        
         $loan = DB::table('loan')->where('loan_id',$id)->get();
         $list = DB::table('book')->get();
-        // passing data pegawai yang didapat ke view edit.blade.php
+        
         return view('editloan',['loan' => $loan,'list'=>$list]);
     
     }
 
     public function update(Request $request)
     {
-        // update data pegawai
+       
         DB::table('loan')->where('loan_id',$request->id)->update([
             'title' => $request->title,
             'member' => $request->member,
             'borrow_date' => $request->borrow_date,
             'return_date' => $request->return_date
         ]);
-        // alihkan halaman ke halaman pegawai
+       
         return redirect('/loan');
     }
 
     public function delete($id)
     {
-        // menghapus data pegawai berdasarkan id yang dipilih
+        
         DB::table('loan')->where('loan_id',$id)->delete();
             
-        // alihkan halaman ke halaman pegawai
+     
         return redirect('/loan');
     }
 
